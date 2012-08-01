36ade575323aee8cd16de480b952de4a036699e5
Nullable <admin@nullable.se>
2012-08-01 05:57:25 +0200

    Remove traces of the old auth code

--- glass/server/protocols.py
+++ glass/server/protocols.py
@@ -3,16 +3,8 @@ from twisted.protocols.basic import LineReceiver
 from .verification import verifyDirectWrapperCert, verifyDirectClientCert
 
 
-STATE_AUTHING = 0
-STATE_OPEN = 1
-
-
 class WrapperProtocol(LineReceiver):
-    AUTH_FAILED = "AUTH_FAILED"
-    AUTH_SUCCESS = "AUTH_SUCCESS"
-
     def __init__(self):
-        self.state = STATE_AUTHING
         self.clients = set()
         self.id = None
 
@@ -34,11 +26,7 @@ class WrapperProtocol(LineReceiver):
 
 
 class ClientProtocol(LineReceiver):
-    AUTH_FAILED = "AUTH_FAILED"
-    AUTH_SUCCESS = "AUTH_SUCCESS"
-
     def __init__(self):
-        self.state = STATE_AUTHING
         self.wrapper = None
 
     def connectionLost(self, reason):
